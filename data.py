634fd64eb56647cbdace3742ad06cd3e4036f6d6
Afie <afietek@gmail.com>
2020-12-20 00:45:23 -0800

    Quiz game!

---
+++ data.py
@@ -0,0 +1,86 @@
+question_data = [
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The name of the main character of the video game 'The Legend of Zelda', is Zelda.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Music",
+        "type": "boolean", "difficulty": "easy",
+        "question": "The music video to The Buggle's 'Video Killed the Radio Star' "
+                    "was the first music video to broadcast on MTV.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean", "difficulty": "easy",
+        "question": "In 'Super Mario 64', collecting 100 coins on a level will give you a 1-UP.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean", "difficulty": "easy",
+        "question": "The logo for Snapchat is a Bell.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "Faust is a playable character in 'Guilty Gear Xrd Revelator'",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "'Half-Life 2' runs on the Source Engine.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "History", "type": "boolean", "difficulty": "easy",
+     "question": "The United States Department of Homeland Security was formed in response "
+                 "to the September 11th attacks.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "Big the Cat is a playable character in 'Sonic Generations'.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Science & Nature", "type": "boolean", "difficulty": "easy",
+     "question": "Salt is 100% composed of Sodium.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "Shaquille O&#039;Neal appeared in the 1997 film 'Space Jam'.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "The PlayStation was originally a joint project between Sega and Sony "
+                 "that was a Sega Genesis with a disc drive.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "Ewan McGregor did not know the name of the second prequel film of Star Wars during and after filming.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "In the 'Toaru Kagaku no Railgun' anime,  espers can only reach a "
+                 "maximum of level 6 in their abilities.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "George Lucas directed the entire original Star Wars trilogy.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Sports", "type": "boolean", "difficulty": "easy",
+     "question": "In association football, or soccer, a corner kick is when the game restarts "
+                 "after someone scores a goal.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "Valve&#039;s 'Portal' and 'Half-Life' franchises exist "
+                 "within the same in-game universe.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Geography", "type": "boolean", "difficulty": "easy",
+     "question": "New Haven is the capital city of the state of Connecticut in the United States.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
+     "question": "Michael Jackson wrote The Simpsons song 'Do the Bartman'.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
+     "question": "The 2011 movie 'The Adventures of Tintin' was directed by Steven Spielberg.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "In Until Dawn, both characters Sam and Mike cannot be killed under any means "
+                 "until the final chapter of the game.",
+     "correct_answer": "True", "incorrect_answers": ["False"]}
+]
